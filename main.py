ab15a3ceddcff139b753e2a4dc683f39b8ff0814
MMaffi <maffimorais@outlook.com>
2026-06-22 15:03:48 -0300

    nova versao

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ DEFAULT_KEEP_BACKUPS = 5
 REPORTS_DIR = DATA_DIR / "Relatórios"
 
 # Constantes para controle de versão
-APP_VERSION = "2025.12.29.1110"
+APP_VERSION = "2026.06.22.1502"
 VERSION_CHECK_URL = "https://raw.githubusercontent.com/MMaffi/gerenciador_firebird/main/version.json"
 
 # Opções disponíveis de pageSize
